8602cbf3992a41d500ba93971ba319f36e18f687
unknown <fajarmh2@gmail.com>
2022-01-26 02:52:09 +0700

    Daily Routine

---
+++ Easy/Reverse_vowels_of_a_string.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        stack<char> st;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(tolower(s[i]) == 'a' || tolower(s[i]) == 'e' || tolower(s[i]) == 'i' || 
+               tolower(s[i]) == 'o' || tolower(s[i]) == 'u') {
+                st.push(s[i]);
+            }
+        }
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(tolower(s[i]) == 'a' || tolower(s[i]) == 'e' || tolower(s[i]) == 'i' || 
+               tolower(s[i]) == 'o' || tolower(s[i]) == 'u') {
+                s[i] = st.top();
+                st.pop();
+            }
+        }
+        
+        return s;
+    }
+};
